54afe29dbf040408ce891f7c0642eec2e6461ce3
VanLin <vanlin1225@gmail.com>
2018-01-01 15:12:05 +0800

    springBootSample package rename

--- SpringBootSample/src/main/java/springBootSample/controller/SampleController.java
+++ SpringBootSample/src/main/java/springBootSample/controller/SampleController.java
@@ -1,4 +1,4 @@
-package springBootTest.controller;
+package springBootSample.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
