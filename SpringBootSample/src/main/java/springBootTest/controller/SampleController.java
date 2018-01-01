3d2a42cc71e5860f705abd41c48232bbaf8e1329
VanLin <vanlin1225@gmail.com>
2018-01-01 15:05:49 +0800

    SpringBootSample

---
+++ SpringBootSample/src/main/java/springBootTest/controller/SampleController.java
@@ -0,0 +1,12 @@
+package springBootTest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+    @RequestMapping("/index")
+    String index() {
+        return "Spring Boot Test";
+    }
+}
